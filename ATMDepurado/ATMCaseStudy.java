745ced8809615d50b22857afa81cd917c3c4adda
EBERTH WILFREDO AZURIN ZUÑIGA <88208531+eazurin@users.noreply.github.com>
2024-05-25 17:49:18 -0500

    Codigo Depurado

--- ATMDepurado/ATMCaseStudy.java
+++ ATMDepurado/ATMCaseStudy.java
@@ -1,1 +1,15 @@
+// ATMCaseSTudy.java
+// Driver program for the ATM case study
+package ATMCaseStudy;
 
+public class ATMCaseStudy {
+	public static void main(String[] args) {
+		try{
+			ATM theATM = new ATM();
+			theATM.run();
+		}
+		catch(Exception e){
+			System.out.println("Error, exited the program....");
+		}
+	}
+}
---
+++ ATMDepurado/Screen.java
@@ -0,0 +1,17 @@
+//Screen.java
+// Represents the screen of the ATM
+package ATMCaseStudy;
+
+public class Screen {
+	public void displayMessage(String message) {
+		System.out.print(message);
+	}
+	
+	public void displayMessageLine(String message) {
+		System.out.println(message);
+	}
+	
+	public void dispalyDollarAmount(double amount) {
+		System.out.printf("$%,.2f", amount);
+	}
+}// end class screen
